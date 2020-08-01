d4848e0483ed9fc67170a6f3cf3849e4e48d951f
Esteban Raffo <e.fraffo@gmail.com>
2020-08-01 19:57:35 -0300

    1/08

--- src/components/datefilter.js
+++ src/components/datefilter.js
@@ -8,7 +8,7 @@ class Datefilter extends Component {
     return (
       <div className="field">
         <div className="control has-icons-left">
-          <input className="input" type="date" min="2020-04-20" max="2020-06-30" value={date.format("YYYY-MM-DD")} onChange={ this.props.onDateChange } name={ this.props.name }/>
+          <input className="input" type="date" min="2020-04-20" max="2020-09-30" value={date.format("YYYY-MM-DD")} onChange={ this.props.onDateChange } name={ this.props.name }/>
           <span className="icon is-small is-left">
             <i className={`fas ${icon}`}></i>
           </span>
